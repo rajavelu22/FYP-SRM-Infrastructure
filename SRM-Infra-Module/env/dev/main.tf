4226819c1e4e74529dc486b68e678d274d284ae8
rajavelugithub <rajavelujansi@gmail.com>
2026-03-26 17:54:51 +0530

    Before go2vicky.online

--- SRM-Infra-Module/env/dev/main.tf
+++ SRM-Infra-Module/env/dev/main.tf
@@ -1,11 +1,10 @@
 terraform {
-  required_version = ">= 1.6.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+  backend "s3" {
+    bucket         = "rajavelu-terraform-state"
+    key            = "alb-project/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
   }
 }
 
